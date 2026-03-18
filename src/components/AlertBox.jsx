06f61685eb17a6c19a7bebd7683fdb88935df5e0
Jayasurya <bjayasurya01@gmail.com>
2026-03-18 16:55:31 +0530

    Alert message

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const Alert = ({ type, icon, children }) => {
   const [show, setShow] = useState(true);
